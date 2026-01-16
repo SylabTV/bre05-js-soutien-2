70f9393ed86f5d92d8a775a921c786a61a0de0a4
sylabtv <sylab.studio@gmail.com>
2026-01-16 16:03:57 +0100

    Fonction avec retour de valeur﻿

---
+++ Exercice-4/script.js
@@ -0,0 +1,11 @@
+let nombres = [10, 25, 5, 40, 18, 3]
+
+
+function afficherGrandsNombres(nombres){
+    for(let i = 0;i < nombres.length; i++){
+        if(nombres[i] > 15){
+            console.log(nombres[i])
+        }
+    }
+}
+afficherGrandsNombres(nombres);
